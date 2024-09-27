c46be9f41fb171aba8a4864fae25a659652b960a
Patrick Goldinger <patrick@patrickgold.dev>
2024-09-27 22:53:07 +0200

    Update emojicon.py script

--- emojicon.py
+++ emojicon.py
@@ -12,7 +12,6 @@ from src.emoji import parse_emoji_test_file, write_emoji_data_file
 
 BUILD_PATH = "build"
 CLDR_PATH = f"{BUILD_PATH}/cldr"
-CLDR_VERSION = "44.1" # specify version in format major or major.minor
 
 ANNOTATIONS_DIR_PATH = f"{CLDR_PATH}/common/annotations"
 ANNOTATIONS_DERIVED_DIR_PATH = f"{CLDR_PATH}/common/annotationsDerived"
@@ -29,12 +28,20 @@ def download_cldr(version: str):
         print(f"CLDR release {version} already downloaded")
     else:
         print(f"Downloading CLDR release {version}")
-        subprocess.run(["wget", "-q", "--show-progress", f"https://github.com/unicode-org/cldr/archive/refs/tags/release-{version_git}.tar.gz", "-P", BUILD_PATH])
+        result = subprocess.run(["wget", "-q", "--show-progress", f"https://github.com/unicode-org/cldr/archive/refs/tags/release-{version_git}.tar.gz", "-P", BUILD_PATH])
+        if result.returncode != 0:
+            print(f"[ERR] Failed to download CLDR release {version}")
+            print(f"[ERR] wget return code: {result.returncode}")
+            sys.exit(1)
     if os.path.exists(CLDR_PATH):
         shutil.rmtree(CLDR_PATH)
     os.makedirs(CLDR_PATH, exist_ok=True)
     print(f"Extracting CLDR release {version} into {CLDR_PATH}")
-    subprocess.run(["tar", "-xf", f"{BUILD_PATH}/release-{version_git}.tar.gz", "-C", CLDR_PATH, "--strip-components=1"])
+    result = subprocess.run(["tar", "-xf", f"{BUILD_PATH}/release-{version_git}.tar.gz", "-C", CLDR_PATH, "--strip-components=1"])
+    if result.returncode != 0:
+        print(f"[ERR] Failed to extract CLDR release {version}")
+        print(f"[ERR] tar return code: {result.returncode}")
+        sys.exit(1)
 
 
 def main(argv: list[str]):
@@ -46,8 +53,12 @@ def main(argv: list[str]):
                 for line in f_readme.readlines():
                     print(line, end="")
         case "build":
-            download_cldr(CLDR_VERSION)
-            lang_code = argv[2] if len(argv) > 2 else LANG_CODE_ROOT
+            cldr_version = argv[2] if len(argv) > 2 else None
+            if cldr_version is None:
+                print("[ERR] Missing CLDR version")
+                sys.exit(1)
+            download_cldr(cldr_version)
+            lang_code = argv[3] if len(argv) > 3 else LANG_CODE_ROOT
             emoji_data = parse_emoji_test_file(path=EMOJI_TEST_FILE_PATH)
             cldr_annotation_mapping: CldrAnnotationMapping
             if lang_code == LANG_CODE_ROOT:
@@ -56,13 +67,17 @@ def main(argv: list[str]):
                 cldr_annotation_mapping = \
                     parse_cldr_annotation_file(f"{ANNOTATIONS_DIR_PATH}/{lang_code}.xml") | \
                     parse_cldr_annotation_file(f"{ANNOTATIONS_DERIVED_DIR_PATH}/{lang_code}.xml")
-            out_dir = EMOJI_ASSETS_OUTPUT_DIR_PATH.format(version=CLDR_VERSION)
+            out_dir = EMOJI_ASSETS_OUTPUT_DIR_PATH.format(version=cldr_version)
             if not os.path.exists(out_dir):
                 os.makedirs(out_dir)
             out_path = f"{out_dir}/{lang_code}.txt"
-            write_emoji_data_file(out_path, emoji_data, cldr_annotation_mapping)
+            write_emoji_data_file(out_path, emoji_data, cldr_annotation_mapping, cldr_version)
         case "buildall":
-            download_cldr(CLDR_VERSION)
+            cldr_version = argv[2] if len(argv) > 2 else None
+            if cldr_version is None:
+                print("[ERR] Missing CLDR version")
+                sys.exit(1)
+            download_cldr(cldr_version)
             emoji_data = parse_emoji_test_file(path=EMOJI_TEST_FILE_PATH)
             cldr_annotation_mappings: dict[str, CldrAnnotationMapping] = dict()
             cldr_annotation_mappings[LANG_CODE_ROOT] = CldrAnnotationMapping()  # empty mapping for root
@@ -74,7 +89,7 @@ def main(argv: list[str]):
                 if lang_code not in cldr_annotation_mappings:
                     cldr_annotation_mappings[lang_code] = CldrAnnotationMapping()
                 cldr_annotation_mappings[lang_code] |= mapping
-            out_dir = EMOJI_ASSETS_OUTPUT_DIR_PATH.format(version=CLDR_VERSION)
+            out_dir = EMOJI_ASSETS_OUTPUT_DIR_PATH.format(version=cldr_version)
             if not os.path.exists(out_dir):
                 os.makedirs(out_dir)
             for lang_code, mapping in cldr_annotation_mappings.items():
@@ -82,9 +97,9 @@ def main(argv: list[str]):
                     continue
                 out_path = f"{out_dir}/{lang_code}.txt"
                 if lang_code == LANG_CODE_ROOT:
-                    write_emoji_data_file(out_path, emoji_data, CldrAnnotationMapping())
+                    write_emoji_data_file(out_path, emoji_data, CldrAnnotationMapping(), cldr_version)
                 else:
-                    write_emoji_data_file(out_path, emoji_data, mapping)
+                    write_emoji_data_file(out_path, emoji_data, mapping, cldr_version)
         case "clean":
             shutil.rmtree("prebuilt")
         case _:
